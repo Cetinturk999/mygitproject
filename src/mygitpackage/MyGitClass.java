83048c9d08ac11e1991858a16279fadb021374ab
Cetinturk999 <rumeysacetinturk90@gmail.com>
2021-04-10 10:12:26 +0300

    yerel kodlarım

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -6,8 +6,13 @@ public class MyGitClass {
 		System.out.println("Commit1");
 		System.out.println("uzak sunucuda ki kodum");
 		System.out.println("uzak sunucuda ki kodum1111111");
-                System.out.println("deneme");
-                System.out.println("java guzeldir");
+        System.out.println("deneme");
+        System.out.println("java guzeldir");
+        System.out.println("11. satýr git kodum");
+        
+        
+        
+        
 	}
 
 }
