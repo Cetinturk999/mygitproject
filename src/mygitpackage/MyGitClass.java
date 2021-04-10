1cf9fc0311e3867d5fbab74358d90e7b2e2c7946
Cetinturk999 <81886269+Cetinturk999@users.noreply.github.com>
2021-04-10 09:40:38 +0300

    Update MyGitClass.java
    update111

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,7 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Commit1");
 		System.out.println("uzak sunucuda ki kodum");
-		
+		System.out.println("uzak sunucuda ki kodum1111111");
 
 	}
 
